a3d269f222c1d9ad6b84ee82d0a1e4ea78a4c26d
Kishor Gunithi <kishorgunithi067@gmail.com>
2026-01-29 22:54:50 +0530

    Refactor user validation logic and improve output messages

--- Day-1/User_Validation.py
+++ Day-1/User_Validation.py
@@ -3,38 +3,32 @@ eMail=input("Enter the Email ID:");
 number=input("Enter the Mobile Number:");
 age=int(input("Enter the age:"));
 
-isName=True;
-isEmail=True;
-isNumber=True;
-isAge=True;
+isName=True
+isEmail=True
+isNumber=True
+isAge=True
+
 """ User Name validation """
 if fullName[0]==" " or fullName[len(fullName)-1]==" ":
     isName=False;
 elif fullName.count(" ")==0:
     isName=False;
-else:
-    isName=True;
 
 """ Email ID validation """
 if(not len(eMail) or " " in eMail or eMail[0]=="." or eMail[0]=="@" or "@" not in eMail or "." not in eMail):
     isEmail=False;
-else:
-    isEmail=True;
 
 """ Number validation """
 if(len(number)!=10 or not number.isdigit() or number[0]=="0"):
     isNumber=False;
-else:
-    isNumber=True;
 
 """ Age validation """
-if(age>=18 and age<=60):
-    isAge=True;
-else:
+if(age<18 or age>60):
     isAge=False;
+
 
 """ Validating Data """
 if(isName and isEmail and isNumber and isAge):
-    print("User Profile is Valid");
+    print("User Profile is VALID");
 else:
-    print("User Profile is InValid");
+    print("User Profile is INVALID");
