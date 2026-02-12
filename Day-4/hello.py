42d10602e2e895e0dc7434d6e578a9b22d84bb06
KISHORGUNITHI <kishorgunithi067@gmail.com>
2026-02-12 22:24:26 +0530

    Day-4

---
+++ Day-4/hello.py
@@ -0,0 +1,35 @@
+n=int(input("Enter the size of the list:"));
+data_list=[0]*n;
+nums=[];
+strings=[];
+print("Enter the data:")
+for i in range(n):
+    data_list[i]=input(str(i+1)+ ". ");
+for data in data_list:
+    if data.isdigit():
+        nums+=[int(data)]
+    elif data!="":
+        strings+=[data]
+
+#choosing option-A
+#As my Registration number last digit is even 
+print("Your nums list:",nums,"\n");
+print("Your strings list:",strings,"\n");
+j=len(nums)-1
+for i in range(len(nums)):
+    if(i>=j): 
+        break
+    else:
+        temp=nums[i]
+        nums[i]=nums[j]
+        nums[j]=temp
+        j=j-1
+
+print("Selecting OPTION_A:REGISTER NUMBER LOGIC")
+print("As my REGISTRATION NUMBER(AP24110011724) last digit is EVEN!!!\n")
+print("I am gonna do REVERSE the NUMS LIST")
+print("REVERSED LIST:",nums,"\n")
+
+#Final Summary
+print("Total Numbers:",len(nums))
+print("Total Strings:",len(strings))
