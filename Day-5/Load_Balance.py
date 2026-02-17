ded858286eb1d368de7ac77c6c2502be9eacd746
KISHORGUNITHI <kishorgunithi067@gmail.com>
2026-02-17 22:53:04 +0530

    added Final_Plan list acc. to PLI

--- Day-5/Load_Balance.py
+++ Day-5/Load_Balance.py
@@ -21,18 +21,38 @@ for weight in weights:
         very_light.append(weight)
     elif weight<=25:
         normal_load.append(weight)
-    elif weight<60:
+    elif weight<=60:
         heavy_load.append(weight)
     else:
         overload.append(weight)
 
 # Applying PLI
-# My_Name=KISHORGUNITHI
-# L=13
-# L%3=1
+My_Name="KISHORGUNITHI"
+L=len(My_Name)
+PLI=L%3;
 
-print(weights)
+final_plan = []
+# removes very_light only
+for weight in weights:
+    if weight>5:        
+        final_plan.append(weight)
 
-for vl_weight in very_light:
-    if vl_weight in weights:
-        weights.remove(vl_weight)
+
+# counting Total valid weights
+valid_count=n-len(invalid_entries)
+print("\nNo.of Valid_Entries are:",valid_count)
+# Counting affected items due to PLI
+affected=len(very_light)
+print("\nNo.of affected entries due to PLI are:",affected)
+
+# Display L and PLI
+print(f"\nL={L} and PLI={PLI}")
+
+print("\nVery Light List:",very_light)
+print("Normal Load List:",normal_load)
+print("Heavy Load List:",heavy_load)
+print("Over Load List:",overload)
+print("Invalid-Entries List:",invalid_entries)
+
+# Display Final Plan
+print("\nFinal Plan List:",final_plan)
